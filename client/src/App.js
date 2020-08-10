15d0986754127cc63f4148d408cb36c581659742
NCM <nmitchell34@gatech.edu>
2020-08-09 21:29:08 -0400

    added models, adding controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './components/navbar'
+import Navbar from "./components/navbar";
 
 function App() {
   useEffect(() => {});
 
-  return <div className="App">
-    <Navbar />
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const query = "";
+const key = "&key=AIzaSyDBrljBHCaJ1VrrzLdhnrrz-_ZQv5UY2y8";
+const queryString =
+  "https://www.googleapis.com/books/v1/volumes?q=" + query + key;
+
+
+
+  
