0e0c4807b168f9d44cf161b49f77e46ee6724f45
NCM <nmitchell34@gatech.edu>
2020-08-09 21:12:27 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Navbar from './components/navbar'
 
 function App() {
   useEffect(() => {});
 
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Navbar />
+  </div>;
 }
 
 export default App;
